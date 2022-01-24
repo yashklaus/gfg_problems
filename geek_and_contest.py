75c3935985ecb5580ccdffea4b7d68d786cce3da
yashklaus <yaswanth9629144795@gmail.com>
2022-01-24 19:21:46 +0530

    new prog

---
+++ geek_and_contest.py
@@ -0,0 +1,12 @@
+def geekcontest(arr):
+    k = 1
+    for i in arr:
+        if 
+
+        
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int,input().strip().split()))[:n]
+    geekcontest(arr)
